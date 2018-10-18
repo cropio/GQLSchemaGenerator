c7143d25d6e7a6b73765dec3474d8d47f8e560e6
Evgeny Kalashnikov <lumyfly@gmail.com>
2018-10-18 19:20:37 +0300

    Updated GQLSchema version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GQLSchemaGenerator",
     dependencies: [
-        .package(url: "git@github.com:Lumyk/GQLSchema.git", from: "0.0.2"),
+        .package(url: "git@github.com:Lumyk/GQLSchema.git", from: "0.0.4"),
     ],
     targets: [
         .target(
